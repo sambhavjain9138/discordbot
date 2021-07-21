b2d92701f7e6d9e31e3584e5d7215a2986d50a09
Sambhav Jain <sambhavjain9138@gmail.com>
2021-07-21 21:56:25 +0530

    removed config file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js")
 const client= new discord.Client()
-const config= require('./config.json')
+// const config= require('./config.json')
 
 client.on('ready',()=>{
     console.log('client is ready')
